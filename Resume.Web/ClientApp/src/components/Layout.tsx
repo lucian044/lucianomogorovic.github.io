3bd27bc2aafa5e09e97d7f38becc0262c93dd5d1
lmogrovic44 <lmogorovic@unionhomemortgage.com>
2020-04-16 21:12:37 -0400

    Add green strip to top of main content container

--- Resume.Web/ClientApp/src/components/Layout.tsx
+++ Resume.Web/ClientApp/src/components/Layout.tsx
@@ -5,6 +5,7 @@ export default (props: { children?: React.ReactNode }) => (
     <div className='main-grid'>
         <NavMenu/>
         <div className='content-container'>
+            <div className="green-strip"></div>
             <div className='content'>
                 {props.children}
             </div>
